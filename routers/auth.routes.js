846d025efea55c2f436f1dd7376626a9379a5675
kimlong <kimlong5244@gmail.com>
2024-08-20 12:08:52 +0700

    refresh token

--- routers/auth.routes.js
+++ routers/auth.routes.js
@@ -20,6 +20,6 @@ router.post(
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
 
-router.post("/refresh-token", authenticateToken, authController.refreshToken);
+router.post("/refresh-token", authController.refreshToken);
 
 module.exports = router;
